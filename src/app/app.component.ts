4d32a49881b970578bdf8260ea5495c2bc9c0a80
easterbuunny <laura.yan@outlook.fr>
2026-04-21 22:50:58 +0200

    Run test, import is good, need to deploy in github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'profil';
+  title = 'PORTFOLIO';
 }
