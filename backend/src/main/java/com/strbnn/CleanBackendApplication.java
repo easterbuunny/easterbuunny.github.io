2cb1b9f05159eda55a80b78a524f528b301aa392
easterbuunny <laura.yan@outlook.fr>
2026-05-12 21:34:07 +0200

    mettre à jour le gitignore

---
+++ backend/src/main/java/com/strbnn/CleanBackendApplication.java
@@ -0,0 +1,11 @@
+package com.strbnn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanBackendApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(CleanBackendApplication.class, args);
+  }
+}
