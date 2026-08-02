2486d6a0c63ebd687b616ee6d53efd054c769572
easterbuunny <laura.yan@outlook.fr>
2026-08-02 11:44:27 +0200

    finalisation de page experience

--- frontend/src/app/experiences/experiences.component.ts
+++ frontend/src/app/experiences/experiences.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Experience } from '../models/experience.model';
-import { EXPERIENCES } from '../data/mock-experiences';
+import { ExperienceService } from '../services/experience.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-experiences',
@@ -11,8 +12,13 @@ import { EXPERIENCES } from '../data/mock-experiences';
   styleUrl: './experiences.component.scss'
 })
 export class ExperiencesComponent {
+    private readonly experienceService = inject(ExperienceService);
 
-  experiences: Experience[] = EXPERIENCES;
+    experiences$!:Observable<Experience[]>;
+
+  ngOnInit(): void {
+    this.experiences$ = this.experienceService.getExperiences();
+  }
 
   badgeClass(type: Experience['type']): string {
     switch (type) {
